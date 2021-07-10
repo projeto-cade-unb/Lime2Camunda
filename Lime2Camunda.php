3969c91a82da2c438a4b61f2b8b721b74a969f7f
Marcio Junior Vieira <marcio@ambientelivre.com.br>
2021-07-10 04:33:09 -0300

    Add test for menu survey

--- Lime2Camunda.php
+++ Lime2Camunda.php
@@ -55,10 +55,13 @@ class Lime2Camunda extends PluginBase {
 	    'default' => TRUE
         ),
     );
-    
+
     public function init()
     {
+        //$this->subscribe('onPluginRegistration');
         $this->subscribe('afterSurveyComplete', 'StartProcessCamunda');
+        //$this->subscribe('newSurveySettings');
+        //$this->subscribe('beforeSurveySettingsSave');
     }
 
     public function StartProcessCamunda()
@@ -129,8 +132,7 @@ class Lime2Camunda extends PluginBase {
 		}
 
               $resultCamundaArray = json_decode($resultCamunda);
-	      foreach ($resultCamundaArray as $name => $value)
-       		 {
+	      foreach ($resultCamundaArray as $name => $value) {
                      if ( $name == "links" ) { 
                           foreach ( (array)$value as $name2 => $value2) {
 	                          foreach ( (array)$value2 as $name3 => $value3) {
@@ -144,9 +146,163 @@ class Lime2Camunda extends PluginBase {
 				  }
                           }
                      }
-		 }
+		}
 	}
 
     }
+/*
+    public function newSurveySettings()
+    {
+        $event = $this->event;
+        foreach ($event->get('settings') as $name => $value)
+        {
+            // In order use survey setting, if not set, use global, if not set use default 
+            $default=$event->get($name,null,null,isset($this->settings[$name]['default'])?$this->settings[$name]['default']:NULL);
+            $this->set($name, $value, 'Survey', $event->get('survey'),$default);
+        }
+    }
+*/
+/*
+    public function beforeSurveySettings()
+    {
+        $event = $this->event;
+        $event->set("surveysettings.{$this->id}", array(
+            'name' => get_class($this),
+            'settings' => array(
+                'boolean'=>array(
+                    'type'=>'boolean',
+                    'label'=>'A boolean setting',
+                    'help'=>'An help text',
+                    'current' => $this->get('boolean', 'Survey', $event->get('survey'),$this->get('boolean',null,null,$this->settings['boolean']['default'])),
+                ),
+                'checkbox'=>array(
+                    'type'=>'checkbox',
+                    'label'=>'A checkbox setting',
+                    'help'=>'An help text',
+                    'current' => $this->get('checkbox', 'Survey', $event->get('survey'),$this->get('checkbox'))
+                ),
+                'float'=>array(
+                    'type'=>'float',
+                    'label'=>'A float setting',
+                    'help'=>'The pattern is set to  "\d+(\.\d+)?"',
+                    'current' => $this->get('float', 'Survey', $event->get('survey'),$this->get('float',null,null,$this->settings['float']['default'])),
+                ),
+                'html'=>array(
+                    'type'=>'html',
+                    'label'=>'A html setting',
+                    'help'=>'Some help for HTML5 editor"',
+                    'current' => $this->get('html', 'Survey', $event->get('survey'),$this->get('html',null,null,$this->settings['html']['default'])),
+                ),
+                'int'=>array(
+                    'type'=>'int',
+                    'label'=>'A int setting',
+                    'help'=>'For integer value, pattern is "\d+"',
+                    'current' => $this->get('int', 'Survey', $event->get('survey'),$this->get('int',null,null,$this->settings['int']['default'])),
+                ),
+                'json'=>array(
+                    'type'=>'json',
+                    'label'=>'A json setting',
+                    'editorOptions'=>array('mode'=>'tree'),
+                    'help'=>'For json settings, here with \'editorOptions\'=>array(\'mode\'=>\'tree\'), . See jsoneditoronline.org',
+                    'current' => $this->get('json', 'Survey', $event->get('survey'),$this->get('json',null,null,$this->settings['json']['default'])),
+                ),
+                'logo'=>array(
+                    'type'=>'logo',
+                    'label'=> 'A logo with a label',
+                    'path'=>Yii::app()->baseUrl."/plugins/exampleSettings/assets/logo.png",
+                ),
+                'info' => array(
+                    'type' => 'info',
+                    'content' => 'Some information to show to admin. You can use html code like <strong>strong</strong> or more with bootstrap for example',
+                ),
+                'relevance'=>array(
+                    'type'=>'relevance',
+                    'label'=>'A relevance setting',
+                    'help'=>'A relevance setting',
+                    'current' => $this->get('relevance', 'Survey', $event->get('survey'),$this->get('relevance')),
+                ),
+                'select'=>array(
+                    'type'=>'select',
+                    'label'=>'A select setting',
+                    'options'=>array(
+                        'opt1'=>'Option 1',
+                        'opt2'=>'Option 2',
+                        'opt3'=>'Option 3 (default)',
+                    ),
+                    'help'=>'A select setting, need an array of option.',
+                    'current' => $this->get('select', 'Survey', $event->get('survey'),$this->get('select',null,null,$this->settings['select']['default'])),
+                ),
+                'string'=>array(
+                    'type'=>'string',
+                    'label'=>'A string setting',
+                    'help'=>'Some help.',
+                    'current' => $this->get('string', 'Survey', $event->get('survey'),$this->get('string',null,null,$this->settings['string']['default'])),
+                ),
+                'text'=>array(
+                    'type'=>'text',
+                    'label'=>'A text setting',
+                    'help'=>'Some help.',
+                    'current' => $this->get('text', 'Survey', $event->get('survey'),$this->get('text',null,null,$this->settings['text']['default'])),
+                ),
+                'password'=>array(
+                    'type'=>'password',
+                    'label'=>'A password setting',
+                    'help'=>'Some help.',
+                    'current' => $this->get('password', 'Survey', $event->get('survey'),$this->get('password',null,null,$this->settings['password']['default'])),
+                ),
+                'date' => array(
+                    'type' => 'date',
+                    'label' => 'A date',
+                    'Help' => 'Date help',
+                    'current' => $this->get('password', 'Survey', $event->get('survey'),$this->get('password',null,null,''))
+                )
+            )
+         ));
+    }
+*/
+
+/*
+ public function onPluginRegistration(){
+    $pluginName = get_class();
+    tracevar($pluginName); // This don't see to happen
+    $thisMenu = Surveymenu::model()->find("title = :pluginName",array(":pluginName"=>$pluginName));
+    if(!$thisMenu){
+      //Check if the menu/menu entries are already created use yii database methods for that
+      
+      $menuArray = array(
+          "parent_id" => 1, //1 -> main surveymenu, 2-> quickemenu, NULL -> new base menu in sidebar
+          "title" => $pluginName,
+          "position" => "side", // possible positions are "side" and "collapsed" state 3.0.0.beta-2
+          "description" => "[your plugins menu description]"
+      );
+      $newMenuId = Surveymenu::staticAddMenu($menuArray);
+
+      // repeat this as often as you need it
+      $menuEntryArray = array(
+          "name" => "[name of the action]",
+          "title" => "[title for the action]",
+          "menu_title" => "[title for the action]", 
+          "menu_description" => "[description for the action]",
+          "menu_icon" => "[icon for action]", //it is either the fontawesome classname withot the fa- prefix, or the iconclass classname, or a src link
+          "menu_icon_type" => "fontawesome", // either 'fontawesome', 'iconclass' or 'image'
+          "menu_link" => "[admin/]controller/sa/action", //the link will be parsed through yii's createURL method
+          "addSurveyId" => true, //add the surveyid parameter to the url
+          "addQuestionGroupId" => true, //add gid parameter to url
+          "addQuestionId" => true, //add qid parameter to url
+          "linkExternal" => false, //open link in a new tab/window
+          "hideOnSurveyState" => null, //possible values are "active", "inactive" and null
+          "manualParams" => "" //please read up on this setting as it may render the link useless
+      );
+      SurveymenuEntries::staticAddMenuEntry($newMenuId, $menuEntryArray);
+    }
+
+  }
+
+*/
+
+
+
+
+
 
 }
